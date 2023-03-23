3430e95bb4ff70300d085f832de5771a69402eb0
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-23 22:56:44 +0900

    Issue1~6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,6 +36,7 @@ class CalendarsController < ApplicationController
         
       end
       
+      
       wday_num = (@todays_date + x).wday
       if  wday_num > 7
         wday_num = wday_num -7
